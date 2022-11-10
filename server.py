959dc642f3f731f877bfbb578d4e89cec8362c03
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2022-11-10 17:09:24 +0000

    Fix Bottle server

--- server.py
+++ server.py
@@ -4,26 +4,23 @@ from main import bmi_calculator
 
 app = Bottle()
 
-@route('/')
+@app.route('/')
 def question():
     return template('question')
 
-@route("/form")
+@app.route("/form")
 def form():
     return template('form')
 
-@route('/calculate')
+@app.route('/calculate')
 def calculate():
     weight = request.query.weight
     height = request.query.height
     bmi = bmi_calculator (weight, height)
     return template('calculate', bmi = bmi)
 
-
-
-
-@get('/style.css')
+@app.get('/style.css')
 def stylesheet():
     return static_file ('style.css', root='static/css')
 
-app.run()
+#app.run()
